bcd5f858319587ccdbdb56837443c814cd9b57ee
Veinz Alucard <65653624+khoidh24@users.noreply.github.com>
2025-02-11 22:49:43 +0700

    Update completions.lua

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,157 +1,157 @@
 return {
-  -- Completion tool for LSP
-  "hrsh7th/cmp-nvim-lsp",
+	-- Completion tool for LSP
+	"hrsh7th/cmp-nvim-lsp",
 
-  -- LuaSnip - Snippets for languages
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-      "hrsh7th/cmp-cmdline",
-    },
-  },
+	-- LuaSnip - Snippets for languages
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-cmdline",
+		},
+	},
 
-  -- Nvim-cmp - Completions UI
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "luckasRanarison/tailwind-tools.nvim",
-      "onsails/lspkind.nvim",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-      luasnip.filetype_extend("javascriptreact", { "html" })
-      luasnip.filetype_extend("typescriptreact", { "html" })
-      require("luasnip.loaders.from_vscode").lazy_load()
-      cmp.setup({
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered({
-            border = { "󱃖", "─", "╮", "│", "", "─", "╰", "│" },
-            winhighlight = "Normal:pumblend,FloatBorder:pumblend,Search:pumblend",
-            col_offset = -4,
-            side_padding = 0,
-            scrollbar = false,
-          }),
-          documentation = cmp.config.window.bordered({
-            border = { "", "─", "╮", "│", "", "─", "╰", "│" },
-            winhighlight = "Normal:pumblend,FloatBorder:pumblend,Search:pumblend",
-          }),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp", priority = 2 },
-          { name = "luasnip", priority = 1 },
-        }),
-        formatting = {
-          fields = { "kind", "abbr" },
-          format = function(entry, vim_item)
-            lspkind.init({
-              symbol_map = {
-                Text = "  ",
-                Method = "  ",
-                Function = "  ",
-                Constructor = "  ",
-                Field = "  ",
-                Variable = "  ",
-                Class = "  ",
-                Interface = "  ",
-                Module = "  ",
-                Property = "  ",
-                Unit = "  ",
-                Value = "  ",
-                Enum = "  ",
-                Keyword = "  ",
-                Snippet = "  ",
-                Color = "󰝤  ",
-                File = "  ",
-                Reference = "  ",
-                Folder = "  ",
-                EnumMember = "  ",
-                Constant = "  ",
-                Struct = "  ",
-                Event = "  ",
-                Operator = "  ",
-                TypeParameter = "  ",
-              },
-            })
-            local kind = lspkind.cmp_format({
-              mode = "symbol",
-              maxwidth = 50,
-              ellipsis_char = "...",
-              show_labelDetails = true,
-              before = require("tailwind-tools.cmp").lspkind_format,
-            })(entry, vim_item)
-            local source = entry.source.name
-            local strings = vim.split(kind.kind, "%s", { trimempty = true })
-            kind.kind = " " .. (strings[1] or "") .. " "
-            if source == "nvim_lsp" or source == "luasnip" then
-              local lspserver_name = nil
-              pcall(function()
-                lspserver_name = entry.source.source.client.name
-                vim_item.menu = lspserver_name
-              end)
-            end
-            return kind
-          end,
-        },
-        cmp.setup.cmdline(":", {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = "path" },
-          }, {
-            {
-              name = "cmdline",
-              option = {
-                ignore_cmds = { "Man", "!" },
-              },
-            },
-          }),
-        }),
-      })
-    end,
-  },
+	-- Nvim-cmp - Completions UI
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"luckasRanarison/tailwind-tools.nvim",
+			"onsails/lspkind.nvim",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local lspkind = require("lspkind")
+			luasnip.filetype_extend("javascriptreact", { "html" })
+			luasnip.filetype_extend("typescriptreact", { "html" })
+			require("luasnip.loaders.from_vscode").lazy_load()
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered({
+						border = { "󱃖", "─", "╮", "│", "", "─", "╰", "│" },
+						winhighlight = "Normal:pumblend,FloatBorder:pumblend,Search:pumblend",
+						col_offset = -4,
+						side_padding = 0,
+						scrollbar = false,
+					}),
+					documentation = cmp.config.window.bordered({
+						border = { "", "─", "╮", "│", "", "─", "╰", "│" },
+						winhighlight = "Normal:pumblend,FloatBorder:pumblend,Search:pumblend",
+					}),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp", priority = 2 },
+					{ name = "luasnip", priority = 1 },
+				}),
+				formatting = {
+					fields = { "kind", "abbr" },
+					format = function(entry, vim_item)
+						lspkind.init({
+							symbol_map = {
+								Text = "  ",
+								Method = "  ",
+								Function = "  ",
+								Constructor = "  ",
+								Field = "  ",
+								Variable = "  ",
+								Class = "  ",
+								Interface = "  ",
+								Module = "  ",
+								Property = "  ",
+								Unit = "  ",
+								Value = "  ",
+								Enum = "  ",
+								Keyword = "  ",
+								Snippet = "  ",
+								Color = "󰝤  ",
+								File = "  ",
+								Reference = "  ",
+								Folder = "  ",
+								EnumMember = "  ",
+								Constant = "  ",
+								Struct = "  ",
+								Event = "  ",
+								Operator = "  ",
+								TypeParameter = "  ",
+							},
+						})
+						local kind = lspkind.cmp_format({
+							mode = "symbol",
+							maxwidth = 50,
+							ellipsis_char = "...",
+							show_labelDetails = true,
+							before = require("tailwind-tools.cmp").lspkind_format,
+						})(entry, vim_item)
+						local source = entry.source.name
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = " " .. (strings[1] or "") .. " "
+						if source == "nvim_lsp" or source == "luasnip" then
+							local lspserver_name = nil
+							pcall(function()
+								lspserver_name = entry.source.source.client.name
+								vim_item.menu = lspserver_name
+							end)
+						end
+						return kind
+					end,
+				},
+				cmp.setup.cmdline(":", {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = "path" },
+					}, {
+						{
+							name = "cmdline",
+							option = {
+								ignore_cmds = { "Man", "!" },
+							},
+						},
+					}),
+				}),
+			})
+		end,
+	},
 
-  -- Tailwind show color
-  {
-    "luckasRanarison/tailwind-tools.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {
-      document_color = {
-        enabled = true, -- can be toggled by commands
-        kind = "inline", -- "inline" | "foreground" | "background"
-        inline_symbol = "󰝤 ", -- only used in inline mode
-        debounce = 100, -- in milliseconds, only applied in insert mode
-      },
-    },
-  },
+	-- Tailwind show color
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			document_color = {
+				enabled = true, -- can be toggled by commands
+				kind = "inline", -- "inline" | "foreground" | "background"
+				inline_symbol = "󰝤 ", -- only used in inline mode
+				debounce = 100, -- in milliseconds, only applied in insert mode
+			},
+		},
+	},
 
-  -- Copilot - Auto suggestion
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_tab_fallback = ""
-      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-    end,
-  },
+	-- Copilot - Auto suggestion
+	-- {
+	--   "github/copilot.vim",
+	--   config = function()
+	--     vim.g.copilot_no_tab_map = true
+	--     vim.g.copilot_assume_mapped = true
+	--     vim.g.copilot_tab_fallback = ""
+	--     vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+	--   end,
+	-- },
 }
