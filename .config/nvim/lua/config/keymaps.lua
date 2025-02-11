674b2ee26f50444d37b3a079d7daec636d36837b
Veinz Alucard <65653624+khoidh24@users.noreply.github.com>
2025-02-11 22:48:34 +0700

    Create keymaps.lua

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,58 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+vim.g.mapleader = " "
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "db", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- Tab management
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap.set("n", "tw", ":tabclose<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-S-h>", "<C-w><")
+keymap.set("n", "<C-S-l>", "<C-w>>")
+keymap.set("n", "<C-S-k>", "<C-w>+")
+keymap.set("n", "<C-S-j>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+	vim.diagnostic.goto_next()
+end, opts)
+
+-- Save file and quit
+keymap.set("n", "<Leader>w", ":w<Return>", opts)
+keymap.set("n", "<Leader>q", ":q<CR>")
+keymap.set("n", "<Leader>Q", ":qa<CR>")
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Disable arrow keys
+keymap.set("", "<up>", "<nop>", opts)
+keymap.set("", "<down>", "<nop>", opts)
+keymap.set("", "<left>", "<nop>", opts)
+keymap.set("", "<right>", "<nop>", opts)
