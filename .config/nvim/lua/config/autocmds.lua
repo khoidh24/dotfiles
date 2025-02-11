6c8818044e1ac6079db619f910ffadcf9661dcbf
Veinz Alucard <65653624+khoidh24@users.noreply.github.com>
2025-02-11 22:47:47 +0700

    Create autocmds.lua

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
